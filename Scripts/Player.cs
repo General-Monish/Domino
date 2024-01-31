d17f0527b860e6148acd038d9e7ea74e81049f36
General-Monish <monissam28@gmail.com>
2024-01-31 16:16:55 +0530

    Added Force  on first domino and created floor

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    private Rigidbody rb;
+    [SerializeField] private float Force;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(0, 0, 1*Force, ForceMode.Impulse);
+        }
     }
 }
